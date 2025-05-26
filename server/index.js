82fe0327c4ba5b52a6d150c277f5a225a59e51ef
rfpg-git <peter.tyrach@googlemail.com>
2025-05-26 13:11:55 +0200

    Connect to mongodb Atlas in the cloud

--- server/index.js
+++ server/index.js
@@ -35,8 +35,8 @@ _.start = () => {
         //mongoose.connect('mongodb://localhost:27017/testdb');
         //mongoose.connect('mongodb+srv://petertyrach:<u5mwg5Pk3Q4pKch>@cluster0.mehevcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/testdb');
         //const dbconnectstring= 'mongodb+srv://' + apiKey + '@cluster0.mehevcm.mongodb.net/testdb';
-        const dbconnectstring=mongoose.connect('mongodb+srv://petertyrach:u5mwg5Pk3Q4pKchy@cluster0.mehevcm.mongodb.net/testdb');
-        mongoose.connect(dbconnectstring);
+        mongoose.connect('mongodb+srv://petertyrach:u5mwg5Pk3Q4pKchy@cluster0.mehevcm.mongodb.net/testdb');
+        //mongoose.connect(dbconnectstring);
         const db = mongoose.connection;
 
         db.on('error', err => {
