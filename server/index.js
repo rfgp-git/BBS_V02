5b4d85732ec1dce9db4bf17f50e7f61bd93369f1
rfpg-git <peter.tyrach@googlemail.com>
2025-07-01 11:47:25 +0200

    Provide a json file with closed days

--- server/index.js
+++ server/index.js
@@ -31,16 +31,16 @@ _.start = () => {
     }
 
     try {
-        //const apiKey = process.env.API_KEY;
+        const apiKey = process.env.API_KEY;
         //const apiKey = process.env.DB_CONNECT_KEY; //Secrets from github
-        //console.log("Your API Key is:", apiKey);
-        //const dbconnectstring= 'mongodb+srv://' + apiKey + '@cluster0.mehevcm.mongodb.net/testdb';
+        console.log("Your API Key is:", apiKey);
+        const dbconnectstring= 'mongodb+srv://' + apiKey + '@cluster0.mehevcm.mongodb.net/testdb';
 
-        mongoose.connect('mongodb://localhost:27017/testdb');
+        //mongoose.connect('mongodb://localhost:27017/testdb');
         
         
         //mongoose.connect('mongodb+srv://petertyrach:u5mwg5Pk3Q4pKchy@cluster0.mehevcm.mongodb.net/testdb');
-        //mongoose.connect(dbconnectstring);
+        mongoose.connect(dbconnectstring);
         const db = mongoose.connection;
 
         db.on('error', err => {
